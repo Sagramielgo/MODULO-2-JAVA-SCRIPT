154915bfa7159788329f9f8ede0a6309b6538da9
Sagramielgo <sagramielgo@gmail.com>
2021-01-17 14:07:35 +0100

    add title, photo and text in main.js

--- module-2-lesson-1-exercise-6/main.js
+++ module-2-lesson-1-exercise-6/main.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const mainElement = document.querySelector('.container');
+
+const title = "<h1>Lorem Ipsum </h1>"
+
+const image = "<img src=http://via.placeholder.com/350x150>"
+
+const text = "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>"
+
+
+mainElement.innerHTML = title + image + text;
