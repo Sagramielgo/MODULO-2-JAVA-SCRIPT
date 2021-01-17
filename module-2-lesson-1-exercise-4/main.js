e6c23aca89dabd04b4e88469461248afd3761d54
Sagramielgo <sagramielgo@gmail.com>
2021-01-17 13:09:27 +0100

    change .title content by adding li class=name1 at innerHTML in main.js

---
+++ module-2-lesson-1-exercise-4/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Obtenemos el elemento con el que queremos trabajar usando document.querySelector()
+const titleElement = document.querySelector('.title');
+
+const winner1Element = document.querySelector('.name1');
+const winner2Element = document.querySelector('.name2');
+
+
+// Cambiamos el contenido del elemento, indicando que sea igual al actual, más una nueva palabra añadida
+titleElement.innerHTML = titleElement.innerHTML + winner1Element.innerHTML;
