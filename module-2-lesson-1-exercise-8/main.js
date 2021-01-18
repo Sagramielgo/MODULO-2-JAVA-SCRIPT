eea99ac403fa94e8d38dad87172ddafa51387e0a
Sagramielgo <sagramielgo@gmail.com>
2021-01-18 20:59:43 +0100

    initial commit

---
+++ module-2-lesson-1-exercise-8/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const listElement = document.querySelector('.list');
+
+
+const one = document.querySelector('.btn__one');
+const two = document.querySelector('.btn__two');
+
+two.classList.add('opacity: 0.5');
