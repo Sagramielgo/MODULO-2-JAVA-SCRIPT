afcbc3367a96b0a4a4e1e4c674aa3ae03086fb21
Sagramielgo <sagramielgo@gmail.com>
2021-01-18 21:46:09 +0100

    add styles in main.css and main.js

--- module-2-lesson-1-exercise-8/main.js
+++ module-2-lesson-1-exercise-8/main.js
@@ -6,4 +6,4 @@ const listElement = document.querySelector('.list');
 const one = document.querySelector('.btn__one');
 const two = document.querySelector('.btn__two');
 
-two.classList.add('opacity: 0.5');
+two.classList.add('soft');
