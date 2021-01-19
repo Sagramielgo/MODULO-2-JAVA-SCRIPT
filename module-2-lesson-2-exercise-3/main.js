4463308d1448a4297a46a2fd8b985ec4d68f9fed
Sagramielgo <sagramielgo@gmail.com>
2021-01-19 20:17:20 +0100

    add apple operation

--- module-2-lesson-2-exercise-3/main.js
+++ module-2-lesson-2-exercise-3/main.js
@@ -9,9 +9,13 @@ console.log(priceKiwi * weightKiwi); // esto pinta en consola 10
 
 const pricePear = 2;
 const weightPear = 3;
-console.log(pricePear * weightPear); // esto pinta en consola 6
+console.log(pricePear * weightPear); 
 
 
 const priceGrape = 4;
 const weightGrape = 0.5;
-console.log(priceGrape * weightGrape); // esto pinta en consola 2
+console.log(priceGrape * weightGrape);
+
+const priceApple = 1.80;
+const weightApple = 3;
+console.log(priceApple - weightApple);
