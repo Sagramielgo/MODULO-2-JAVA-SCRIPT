4fda356cdfe9402f665ba5162841345ff1c46e83
Sagramielgo <sagramielgo@gmail.com>
2021-01-19 17:08:49 +0100

    add operations with entegers and float numbers in main.js

--- module-2-lesson-2-exercise-3/main.js
+++ module-2-lesson-2-exercise-3/main.js
@@ -1,2 +1,17 @@
 'use strict';
 
+
+const priceKiwi = 5;
+const weightKiwi = 2;
+console.log(priceKiwi * weightKiwi); // esto pinta en consola 10
+
+
+
+const pricePear = 2;
+const weightPear = 3;
+console.log(pricePear * weightPear); // esto pinta en consola 6
+
+
+const priceGrape = 4;
+const weightGrape = 0.5;
+console.log(priceGrape * weightGrape); // esto pinta en consola 2
