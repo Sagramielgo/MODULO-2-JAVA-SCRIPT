7d70e50ccfc1ef97b347ab119bcc1d6f25ab4f99
Sagramielgo <sagramielgo@gmail.com>
2021-01-20 11:30:45 +0100

    add parseInt in main.js

---
+++ ex-extra-de-clase/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Datos
+// - input capt
+// - span min
+// - section resultado
+
+// Proceso:
+// - Rescatar elementos con los datos
+// - Sacar los datos de los elementos
+// comprobar con debugger
+// -  Hacer multiplicación
+// - Mostrar en html
