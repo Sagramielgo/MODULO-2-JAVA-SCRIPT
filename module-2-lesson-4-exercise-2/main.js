fd62e83df809feea330a09ed4ebf15837fb0ee09
Sagramielgo <sagramielgo@gmail.com>
2021-01-21 22:45:12 +0100

    create media function

--- module-2-lesson-4-exercise-2/main.js
+++ module-2-lesson-4-exercise-2/main.js
@@ -1,7 +1,8 @@
 'use strict';
 const number = document.querySelector(".Marinumber")
 
-function multiply(a, b, c, d) {
-  return a + b + c + d;
+function getMedia(a, b, c, d) {
+  return (a + b + c + d) / 4;
 }
-
+const media = getMedia(4, 7, 5, 8)
+console.log("La media de estos números es:", media);
