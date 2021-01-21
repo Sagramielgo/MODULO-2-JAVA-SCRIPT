59316d989d46727091df2892cbeea120b3ea6208
Sagramielgo <sagramielgo@gmail.com>
2021-01-21 23:52:27 +0100

    add create iva function

--- module-2-lesson-4-exercise-3/main.js
+++ module-2-lesson-4-exercise-3/main.js
@@ -1,3 +1,15 @@
 'use strict';
-const number = document.querySelector(".Marinumber")
 
+
+
+
+function getIva(a) {
+  return "Precio sin IVA: " + a + "€, con IVA: " + a * 0.21 + "€ y Total: " + ((a * 0.21) + a);
+}
+
+
+
+const ivaPrice = getIva(10)
+
+
+console.log(ivaPrice);
