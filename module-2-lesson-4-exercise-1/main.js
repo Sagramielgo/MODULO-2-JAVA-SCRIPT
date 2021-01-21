c85a6526e43f59ffe25d4478de96ce2bb1d25ab4
Sagramielgo <sagramielgo@gmail.com>
2021-01-21 22:32:28 +0100

    declare multiply function

--- module-2-lesson-4-exercise-1/main.js
+++ module-2-lesson-4-exercise-1/main.js
@@ -1,3 +1,10 @@
 'use strict';
 const number = document.querySelector(".Marinumber")
 
+function multiply(a, b) {
+  return a * b;
+}
+
+const price = multiply(4, 5)
+
+console.log("Precio total", price);
