a98c345155ecb8c07c8452ecfa02eec8ad8db554
Sagramielgo <sagramielgo@gmail.com>
2021-01-22 08:54:40 +0100

    add function in main.js

--- module-2-lesson-4-exercise-2/main.js
+++ module-2-lesson-4-exercise-2/main.js
@@ -4,5 +4,6 @@ const number = document.querySelector(".Marinumber")
 function getMedia(a, b, c, d) {
   return (a + b + c + d) / 4;
 }
+
 const media = getMedia(4, 7, 5, 8)
 console.log("La media de estos números es:", media);
--- module-2-lesson-4-exercise-4/main.js
+++ module-2-lesson-4-exercise-4/main.js
@@ -3,3 +3,15 @@
 
 
 
+function  evenOdd(a)  { 
+return (a % 2); 
+ } 
+
+const result = evenOdd(6 % 2);
+
+if ( result===0){
+  console.log("Este número es par");
+}
+else {console.log("Este número es impar");}
+
+ 
