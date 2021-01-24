148267898db8d7179a9d003204a1ce2f971905fd
Sagramielgo <sagramielgo@gmail.com>
2021-01-24 14:05:47 +0100

    add text to paragraph with function mouseover

---
+++ module-2-lesson-5-exercise-3/main.js
@@ -0,0 +1,8 @@
+"user strict";
+
+
+const text = document.querySelector('.text');
+
+
+text.addEventListener('mouseover', function addText () {text.innerHTML = text.innerHTML + " Este texto se acaba de añadir al pasar el ratón por encima con la función MOUSEOVER"}
+);
