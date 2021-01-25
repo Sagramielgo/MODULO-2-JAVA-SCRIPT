f243399bf38507f8e1bd45f01291a27eb98eb39c
Sagramielgo <sagramielgo@gmail.com>
2021-01-25 21:03:28 +0100

    create my first object

---
+++ module-2-lesson-6-exercise-1/main.js
@@ -0,0 +1,25 @@
+"user strict";
+
+
+const adalaber1 = document.querySelector (".description1");
+{
+adalaber1.name = 'Susana';
+adalaber1.age = 34;
+adalaber1.job = "Periodista";
+}
+
+ adalaber1.innerHTML = "Mi nombre es " + adalaber1.name + " tengo " + adalaber1.age + " años y soy " + adalaber1.job +".";
+
+console.log(adalaber1.age);
+
+
+
+const adalaber2 = document.querySelector (".description2");
+{
+adalaber2.name = 'Rocío';
+adalaber2.age = 25;
+adalaber2.job = "Actriz";
+}
+console.log(adalaber2.age);
+
+adalaber2.innerHTML = "Mi nombre es " + adalaber2.name + " tengo " + adalaber2.age + " años y soy " + adalaber2.job +".";
