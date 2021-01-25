a00aa3e215d01c06b9e012bed534ed3072c642bf
Sagramielgo <sagramielgo@gmail.com>
2021-01-25 23:12:50 +0100

    add function with this

--- module-2-lesson-6-exercise-2/main.js
+++ module-2-lesson-6-exercise-2/main.js
@@ -5,12 +5,16 @@ const adalaber1 = document.querySelector (".description1");
 {
 adalaber1.name = 'Susana';
 adalaber1.age = 34;
-adalaber1.job = "Periodista";
+adalaber1.job = "periodista";
 }
  adalaber1.innerHTML = `Mi nombre es ${adalaber1.name} tengo ${adalaber1.age} años y soy ${adalaber1.job}.`;
 
-console.log(adalaber1.age);
+/* console.log(adalaber1.age); */
+
 
 
 adalaber1.run = phrase => `Yo estoy ${phrase}`;
-console.log(adalaber1.run('corriendo'));
+adalaber1.runAMarathon = distance => `una maratón de ${distance} km`;
+ distance = 50;
+console.log(adalaber1.run('corriendo'), adalaber1.runAMarathon
+);
