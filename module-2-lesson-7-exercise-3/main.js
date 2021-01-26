cdefa583bbcf5a8ab31929f8a7cfd8dffeeffe48
Sagramielgo <sagramielgo@gmail.com>
2021-01-26 22:18:00 +0100

    add partial result to bucle

--- module-2-lesson-7-exercise-3/main.js
+++ module-2-lesson-7-exercise-3/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-for (let acc = 0; acc < 11; acc + 2) {
-  console.log('El resultado es ' + acc);
+let acc = 0;
+for (let i = 0; i < 11; i++) {
+  acc = acc + 2;
+
+  console.log(`El índice del bucle es: ${i}`);
+
+  console.log(`El resultado parcial es ${acc}`);
 }
+console.log(`El resultado final es  ${acc}`);
