b88d228b0e306528fee7f169e6818a5bd3e60c20
Sagramielgo <sagramielgo@gmail.com>
2021-01-31 19:23:59 +0100

    test splice array method

---
+++ module-2-lesson-9-exercise-extra- slice/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const names = ['Rita', 'María', 'Lucía', 'Ana', 'Vanesa'];
+console.log(names.slice(1, 3)); // ["María", "Lucía"]
+console.log(names.slice(1)); // ['María', 'Lucía', 'Ana', 'Vanesa']
+
+console.log(names.slice(-2)); // ['Ana', 'Vanesa']
